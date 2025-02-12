bd3e73e1075f552f89839247d65f9b71e9689b66
Sandip622 <sandippatel07072004@gmail.com>
2025-02-12 08:19:40 +0530

    Update rxce.py

--- rxce.py
+++ rxce.py
@@ -12,9 +12,9 @@ from base64 import b64decode,b64encode
 from datetime import date
 
 
-expirydate = datetime.date(2021, 9, 24)
+expirydate = datetime.date(2025, 14, 2)
 #expirydate = datetime.date(2021, 8, 30)
-today=date.today()
+today=date.today(2025,12,2)
 green="\033[3;32m"
 neon="\033[3;36m"
 nc="\033[00m"
